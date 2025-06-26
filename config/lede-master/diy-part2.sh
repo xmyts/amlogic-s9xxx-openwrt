68fb7cf6a58390be0bb4bc777364afdc77944445
xmyts <2687662318@qq.com>
2025-06-26 09:54:19 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -34,8 +34,7 @@ git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # ------------------------------- Other started -------------------------------
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 
