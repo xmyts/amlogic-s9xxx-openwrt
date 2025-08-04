a1220a90e44163380d9d04d09b6f97776e40db1f
xmyts <2687662318@qq.com>
2025-08-04 11:54:31 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -12,3 +12,10 @@
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
+# 更新软件包列表
+sudo apt update
+
+# 安装 help2man
+sudo apt install -y help2man
+sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default 
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
