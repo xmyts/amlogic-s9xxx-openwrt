d68410649d73c9cfd4cb2bd4f1e3fcffb4844e9f
xmyts <2687662318@qq.com>
2025-09-18 17:23:04 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -20,3 +20,4 @@
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default 
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
