0b859c133708bc36a8a6c6250f78fff0ade72cd3
xmyts <2687662318@qq.com>
2025-09-19 10:50:35 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -19,5 +19,5 @@
 
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default 
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
- rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+sed -i '$a src-git qbittorrent https://github.com/JSZMonkey/luci-app-qbittorrent.git'
+git clone https://github.com/noiver/luci-app-jd-dailybonus  package/luci-app-jd-dailybonus
