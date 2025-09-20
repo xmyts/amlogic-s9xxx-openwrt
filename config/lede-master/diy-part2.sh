d6ea2b4fb09b626554cbce1034ecf061b3765835
xmyts <2687662318@qq.com>
2025-09-20 22:37:08 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -25,7 +25,7 @@ rm -f package/feeds/luci/luci-app-gowebdav
 
 
 ./scripts/feeds install -p smpackage qbittorrent qt6base qt6tools rblibtorrent luci-app-qbittorrent
-./scripts/feeds install -p NueXini_Packages luci-app-gowebdav
+#  ./scripts/feeds install -p NueXini_Packages luci-app-gowebdav
 
 # ------------------------------------------------------------------------------------------------------------------------------
 
