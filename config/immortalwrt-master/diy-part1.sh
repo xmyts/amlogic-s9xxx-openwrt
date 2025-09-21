5d1ea31f83e17e8633b71d333ff709a00a6f5cab
xmyts <2687662318@qq.com>
2025-09-21 16:35:16 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -19,3 +19,4 @@ sudo apt update
 # sudo apt install -y help2man
 # sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default 
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+src-git qbittorrent https://github.com/sbwml/luci-app-qbittorrent.git
